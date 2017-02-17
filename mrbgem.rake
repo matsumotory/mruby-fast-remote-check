dcf257bfa09c3d89b766f941b0d9b8e97e991a5c
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2017-02-17 06:36:06 +0000

    Add test for FastRemoteCheck#open_raw?

--- mrbgem.rake
+++ mrbgem.rake
@@ -1,4 +1,5 @@
 MRuby::Gem::Specification.new('mruby-fast-remote-check') do |spec|
   spec.license = 'MIT'
   spec.authors = 'MATSUMOTO Ryosuke'
+  spec.add_test_dependency 'mruby-time', :core => 'mruby-time'
 end
--- test/mrb_fastremotecheck.rb
+++ test/mrb_fastremotecheck.rb
@@ -2,16 +2,24 @@
 ## FastRemoteCheck Test
 ##
 
-assert("FastRemoteCheck#hello") do
-  t = FastRemoteCheck.new "hello"
-  assert_equal("hello", t.hello)
+assert("FastRemoteCheck#port") do
+  # check redis port
+  t = FastRemoteCheck.new "127.0.0.1", 54321, "127.0.0.1", 6379, 3
+  assert_true t.open_raw?
 end
 
-assert("FastRemoteCheck#bye") do
-  t = FastRemoteCheck.new "hello"
-  assert_equal("hello bye", t.bye)
+assert("FastRemoteCheck#port") do
+  # check redis port
+  t = FastRemoteCheck.new "127.0.0.1", 54321, "127.0.0.1", 6380, 3
+  assert_false t.open_raw?
 end
 
-assert("FastRemoteCheck.hi") do
-  assert_equal("hi!!", FastRemoteCheck.hi)
+assert("FastRemoteCheck#port") do
+  # check redis port
+  timeout = 2
+  t = FastRemoteCheck.new "127.0.0.1", 54321, "1.1.1.1", 6380, timeout
+  before = Time.now
+  assert_raise(RuntimeError) { t.open_raw? }
+  after = Time.now
+  assert_true (after - before) < (timeout + 1)
 end
