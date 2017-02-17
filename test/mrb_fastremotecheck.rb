449d07c8e9c35b5b62396785a33b4226952de5d1
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2017-02-17 07:34:40 +0000

    Add test for connect with SO_LINGER

--- test/mrb_fastremotecheck.rb
+++ test/mrb_fastremotecheck.rb
@@ -2,24 +2,46 @@
 ## FastRemoteCheck Test
 ##
 
-assert("FastRemoteCheck#port") do
+assert("FastRemoteCheck#open_raw? for listeing") do
   # check redis port
   t = FastRemoteCheck.new "127.0.0.1", 54321, "127.0.0.1", 6379, 3
   assert_true t.open_raw?
 end
 
-assert("FastRemoteCheck#port") do
+assert("FastRemoteCheck#open_raw? for not linsting") do
   # check redis port
   t = FastRemoteCheck.new "127.0.0.1", 54321, "127.0.0.1", 6380, 3
   assert_false t.open_raw?
 end
 
-assert("FastRemoteCheck#port") do
+assert("FastRemoteCheck#oepn_raw? for ip unreachable") do
   # check redis port
   timeout = 2
   t = FastRemoteCheck.new "127.0.0.1", 54321, "1.1.1.1", 6380, timeout
   before = Time.now
   assert_raise(RuntimeError) { t.open_raw? }
+  after = Time.now
+  assert_true (after - before) < (timeout + 1)
+end
+
+assert("FastRemoteCheck#connect? for listeing") do
+  # check redis port
+  t = FastRemoteCheck.new "127.0.0.1", 54321, "127.0.0.1", 6379, 3
+  assert_true t.connect?
+end
+
+assert("FastRemoteCheck#connect? for not linsting") do
+  # check redis port
+  t = FastRemoteCheck.new "127.0.0.1", 54321, "127.0.0.1", 6380, 3
+  assert_false t.connect?
+end
+
+assert("FastRemoteCheck#oepn_raw? for ip unreachable") do
+  # check redis port
+  timeout = 2
+  t = FastRemoteCheck.new "127.0.0.1", 54321, "1.1.1.1", 6380, timeout
+  before = Time.now
+  assert_raise(RuntimeError) { t.connect? }
   after = Time.now
   assert_true (after - before) < (timeout + 1)
 end
