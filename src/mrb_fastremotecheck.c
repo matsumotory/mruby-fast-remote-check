428f925825d3902243b135c3c40de6bc45b6bc2e
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2017-02-17 06:36:40 +0000

    First implementaion for raw socket port checker

--- src/mrb_fastremotecheck.c
+++ src/mrb_fastremotecheck.c
@@ -6,8 +6,11 @@
 ** See Copyright Notice in LICENSE
 */
 
+#define _GNU_SOURCE
+
 #include "mruby.h"
 #include "mruby/data.h"
+#include "mruby/error.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,8 +28,9 @@
 
 #define SYS_FAIL_MESSAGE_LENGTH 2048
 #define DONE mrb_gc_arena_restore(mrb, 0);
-#define PACKET_FOUND 1
 #define PACKET_NOT_FOUND 0
+#define SYN_ACK_PACKET_FOUND 1
+#define RST_PACKET_FOUND 2
 
 struct pseudo_ip_header {
   unsigned int src_ip;
@@ -43,7 +47,11 @@ struct pseudo_header {
 
 /* mruby-fast-remote-check user data */
 typedef struct {
-  int saddr_size;
+  u_int32_t src_ip;
+  u_int32_t dst_ip;
+  u_int16_t src_port;
+  u_int16_t dst_port;
+  socklen_t saddr_size;
   int tcphdr_size;
   int iphdr_size;
   struct sockaddr *peer_ptr;
@@ -51,8 +59,23 @@ typedef struct {
   struct timeval timeout;
 } mrb_fastremotecheck_data;
 
+static void mrb_fastremotecheck_data_free(mrb_state *mrb, void *p)
+{
+  mrb_fastremotecheck_data *data = (mrb_fastremotecheck_data *)p;
+
+  if (data) {
+    if (data->tcphdr) {
+      mrb_free(mrb, data->tcphdr);
+    }
+    if (data->peer_ptr) {
+      mrb_free(mrb, data->peer_ptr);
+    }
+    mrb_free(mrb, data);
+  }
+}
+
 static const struct mrb_data_type mrb_fastremotecheck_data_type = {
-    "mrb_fastremotecheck_data", mrb_free,
+    "mrb_fastremotecheck_data", mrb_fastremotecheck_data_free,
 };
 
 static void mrb_fastremotecheck_sys_fail(mrb_state *mrb, int error_no, const char *fmt, ...)
@@ -95,14 +118,14 @@ static unsigned short checksum(unsigned short *buffer, int size)
 static mrb_value mrb_fastremotecheck_init(mrb_state *mrb, mrb_value self)
 {
   mrb_fastremotecheck_data *data;
-  const char *src_ip;
-  const char *dst_ip;
+  char *src_ip;
+  char *dst_ip;
   mrb_int src_port;
   mrb_int dst_port;
   mrb_int timeout_arg = 0;
-  struct tcphdr tcphdr;
-  struct pseudo_header pheader;
-  struct sockaddr_in peer;
+  struct tcphdr *tcphdr;
+  struct pseudo_header *pheader;
+  struct sockaddr_in *peer;
   struct timeval timeout;
   timeout.tv_sec = 3; /* default timeout */
   timeout.tv_usec = 0;
@@ -124,40 +147,49 @@ static mrb_value mrb_fastremotecheck_init(mrb_state *mrb, mrb_value self)
     timeout.tv_usec = 0;
   }
 
-  tcphdr.source = htons(src_port);
-  tcphdr.dest = htons(dst_port);
-  tcphdr.window = htons(4321);
-  tcphdr.seq = 1;
-  tcphdr.fin = 0;
-  tcphdr.syn = 1;
-  tcphdr.doff = 5;
-  tcphdr.rst = 0;
-  tcphdr.urg = 0;
-  tcphdr.urg_ptr = 0;
-  tcphdr.psh = 0;
-  tcphdr.ack_seq = 0;
-  tcphdr.ack = 0;
-  tcphdr.check = 0;
-  tcphdr.res1 = 0;
-  tcphdr.res2 = 0;
+  tcphdr = (struct tcphdr *)mrb_malloc(mrb, sizeof(struct tcphdr));
+  memset(tcphdr, 0, sizeof(struct tcphdr));
+  tcphdr->source = htons(src_port);
+  tcphdr->dest = htons(dst_port);
+  tcphdr->window = htons(4321);
+  tcphdr->seq = 1;
+  tcphdr->fin = 0;
+  tcphdr->syn = 1;
+  tcphdr->doff = 5;
+  tcphdr->rst = 0;
+  tcphdr->urg = 0;
+  tcphdr->urg_ptr = 0;
+  tcphdr->psh = 0;
+  tcphdr->ack_seq = 0;
+  tcphdr->ack = 0;
+  tcphdr->check = 0;
+  tcphdr->res1 = 0;
+  tcphdr->res2 = 0;
 
-  inet_aton(src_ip, (struct in_addr *)&pheader.iphdr.src_ip);
-  inet_aton(dst_ip, (struct in_addr *)&pheader.iphdr.dst_ip);
+  pheader = (struct pseudo_header *)alloca(sizeof(struct pseudo_header));
+  memset(pheader, 0, sizeof(struct pseudo_header));
+  inet_aton(src_ip, (struct in_addr *)&pheader->iphdr.src_ip);
+  inet_aton(dst_ip, (struct in_addr *)&pheader->iphdr.dst_ip);
+  pheader->iphdr.zero = 0;
+  pheader->iphdr.protocol = 6;
+  pheader->iphdr.len = htons(sizeof(struct ip));
 
-  pheader.iphdr.zero = 0;
-  pheader.iphdr.protocol = 6;
-  pheader.iphdr.len = htons(sizeof(struct ip));
+  peer = (struct sockaddr_in *)mrb_malloc(mrb, sizeof(struct sockaddr_in));
+  memset(peer, 0, sizeof(struct sockaddr_in));
+  inet_aton(dst_ip, &peer->sin_addr);
+  peer->sin_port = htons(dst_port);
+  peer->sin_family = AF_INET;
 
-  inet_aton(dstip, &peer.sin_addr);
-  peer.sin_port = htons(dst_port);
-  peer.sin_family = AF_INET;
+  bcopy((char *)tcphdr, (char *)&pheader->ptcphdr, sizeof(struct ip));
+  tcphdr->check = checksum((unsigned short *)pheader, 32);
 
-  bcopy((char *)&tcphdr, (char *)&pheader.ptcphdr, sizeof(struct ip));
-  tcphdr.check = checksum((unsigned short *)&pheader, 32);
-
-  data->saddr_size = sizeof(peer);
-  data->peer_ptr = (struct sockaddr *)&peer;
-  data->tcphdr = &tcphdr;
+  data->src_ip = pheader->iphdr.src_ip;
+  data->dst_ip = pheader->iphdr.dst_ip;
+  data->src_port = tcphdr->source;
+  data->dst_port = tcphdr->dest;
+  data->saddr_size = sizeof(struct sockaddr_in);
+  data->peer_ptr = (struct sockaddr *)peer;
+  data->tcphdr = tcphdr;
   data->tcphdr_size = sizeof(struct tcphdr);
   data->iphdr_size = sizeof(struct ip);
   data->timeout = timeout;
@@ -190,14 +222,15 @@ static mrb_int fastremotecheck_create_raw_socket(mrb_state *mrb, struct timeval 
   return sock;
 }
 
-static mrb_int fastremotecheck_found_syn_ack_packet(mrb_fastremotecheck_data *data, unsigned char buffer)
+static mrb_int fastremotecheck_found_syn_ack_packet(mrb_fastremotecheck_data *data, unsigned char *buffer)
 {
-  struct iphdr *iph = (struct iphdr *)buffer;
-  struct tcphdr *tcp = (struct tcphdr *)(buffer + data->iphdr_size);
+  struct iphdr *iphdr = (struct iphdr *)buffer;
+  struct tcphdr *tcphdr = (struct tcphdr *)(buffer + data->iphdr_size);
 
-  if (tcp->syn == 1 && tcp->ack == 1 && tcp->source == data->peer_ptr->sin_port &&
-      iph->saddr == data->peer_ptr->sin_addr.s_addr) {
-    return PACKET_FOUND;
+  if (tcphdr->syn == 1 && tcphdr->ack == 1 && iphdr->saddr == data->dst_ip && tcphdr->source == data->dst_port) {
+    return SYN_ACK_PACKET_FOUND;
+  } else if (tcphdr->rst == 1 && iphdr->saddr == data->dst_ip && tcphdr->source == data->dst_port) {
+    return RST_PACKET_FOUND;
   } else {
     return PACKET_NOT_FOUND;
   }
@@ -206,10 +239,11 @@ static mrb_int fastremotecheck_found_syn_ack_packet(mrb_fastremotecheck_data *da
 static mrb_value mrb_fastremotecheck_port_raw(mrb_state *mrb, mrb_value self)
 {
   mrb_int sock;
+  mrb_int ret;
   mrb_int retry = 0;
   mrb_int max_retry = 5; /* default max retry */
   mrb_fastremotecheck_data *data = DATA_PTR(self);
-  mrb_int saddr_size = data->saddr_size;
+  socklen_t saddr_size = data->saddr_size;
 
   sock = fastremotecheck_create_raw_socket(mrb, data->timeout);
 
@@ -227,10 +261,13 @@ static mrb_value mrb_fastremotecheck_port_raw(mrb_state *mrb, mrb_value self)
       mrb_fastremotecheck_sys_fail(mrb, errno, "recvfrom failed");
     }
 
-    if (fastremotecheck_found_syn_ack_packet(data, buffer) == PACKET_FOUND) {
+    ret = fastremotecheck_found_syn_ack_packet(data, buffer);
+    if (ret == SYN_ACK_PACKET_FOUND) {
       return mrb_true_value();
+    } else if (ret == RST_PACKET_FOUND) {
+      return mrb_false_value();
     }
-    retry++:
+    retry++;
   }
 
   close(sock);
@@ -242,8 +279,8 @@ void mrb_mruby_fast_remote_check_gem_init(mrb_state *mrb)
 {
   struct RClass *fastremotecheck;
   fastremotecheck = mrb_define_class(mrb, "FastRemoteCheck", mrb->object_class);
-  mrb_define_method(mrb, fastremotecheck, "initialize", mrb_fastremotecheck_init, MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, fastremotecheck, "raw", mrb_fastremotecheck_port_raw, MRB_ARGS_NONE());
+  mrb_define_method(mrb, fastremotecheck, "initialize", mrb_fastremotecheck_init, MRB_ARGS_REQ(5));
+  mrb_define_method(mrb, fastremotecheck, "open_raw?", mrb_fastremotecheck_port_raw, MRB_ARGS_NONE());
   DONE;
 }
 
